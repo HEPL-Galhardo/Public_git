55920949a72569991dd8e861ebc01f6920f76492
HEPL-Galhardo <joao.galhardocarraca@student.hepl.be>
2023-11-24 14:52:35 +0100

    Adding comments

--- TP3-MicroPython-UDPvsTCP.py
+++ TP3-MicroPython-UDPvsTCP.py
@@ -1,73 +1,89 @@
-import network
-import socket
-import urequests as requests
-import machine,utime
-import secrets
-led = machine.Pin(2, machine.Pin.OUT)  # GPIO 2 est utilisé pour contrôler la LED
+# Import necessary modules
+import network  # For WiFi connectivity
+import socket   # For handling sockets (TCP and UDP)
+import urequests as requests  # For making HTTP requests (not used in this code)
+import machine  # For hardware-related operations
+import utime    # For handling time-related operations
+import secrets  # A custom module (not included here) for storing sensitive data
+
+# Set up a pin for controlling an LED (GPIO 2 in this case)
+led = machine.Pin(2, machine.Pin.OUT)
 
 #---------------------------------------Wifi Connection-------------------------------------------
-#Wifi connection: setting the identifier and the password to the wifi connection
-SSID= secrets.SSID_1				#Kot_1
-PASSWORD= secrets.PASSWORD_1		#HEPL_2
 
-# Configuration du serveur TCP et UDP
-TCP_SERVER_IP = "192.168.1.26"
-TCP_SERVER_PORT = 5000
-UDP_SERVER_IP = "192.168.1.26"
-UDP_SERVER_PORT = 5070
+# Define WiFi credentials (SSID and PASSWORD) using secrets module
+SSID = secrets.SSID_1        # SSID of the WiFi network
+PASSWORD = secrets.PASSWORD_1    # Password for the WiFi network
 
+# Define TCP and UDP server configurations
+TCP_SERVER_IP = "192.168.1.26"  # IP address of TCP server
+TCP_SERVER_PORT = 5000          # Port for TCP communication
+UDP_SERVER_IP = "192.168.1.26"  # IP address of UDP server
+UDP_SERVER_PORT = 5070          # Port for UDP communication
+
+# Create a WLAN (Wireless Local Area Network) object
 wlan = network.WLAN(network.STA_IF)
-#Enabeling the wlan
+
+# Enable WLAN
 wlan.active(True)
-print("Wifi On, Status:",wlan.status())
-wlan.connect(SSID, PASSWORD) #wlan.connect("secrets.SSID", "secrets.PASSWORD")
-print("Waiting for connection with ",SSID)
-#Loop while the wifi connection isn't establissed
+print("Wifi On, Status:", wlan.status())
+
+# Connect to the WiFi network using the provided SSID and PASSWORD
+wlan.connect(SSID, PASSWORD)
+print("Waiting for connection with ", SSID)
+
+# Loop until the WiFi connection is established
 while not wlan.isconnected():
-    #
-    led.value(1)	#Led off
-    utime.sleep(1)	#Wait for 1 second
-    led.value(0)	#Led on
+    # Blink LED to indicate attempts to connect
+    led.value(1)   # LED off
+    utime.sleep(1)   # Wait for 1 second
+    led.value(0)   # LED on
     utime.sleep(1)
-    #Checking the connection, if connection jus
+    
     try:
         print("...")
         if wlan.isconnected():
-            print("Wifi connection made with ",SSID)
+            print("Wifi connection made with ", SSID)
             break
-    #If the connection isn't working then re-trying after 1 second
     except:
-        print("Wifi connection couldn't be made!, Re-trying...")
+        # If an exception occurs, retry connecting to WiFi
+        print("Wifi connection couldn't be made! Retrying...")
         wlan.connect(SSID, PASSWORD)
         pass
-#Re-check the connection 
-print("Wifi connection made with ",SSID)
-#Show the actual connection features
-print("IP Adresse informations: ",wlan.ifconfig())
 
-led.value(0)  # Allume la LED
+# Re-check the connection
+print("Wifi connection made with ", SSID)
 
-# Configuration du socket TCP
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_address = (TCP_SERVER_IP, TCP_SERVER_PORT)
-tcp_sock.connect(tcp_address)
+# Display the actual connection information
+print("IP Address information: ", wlan.ifconfig())
 
-# Configuration du socket UDP
-udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_address = (UDP_SERVER_IP, UDP_SERVER_PORT)
+led.value(0)  # Turn on the LED to indicate successful connection
 
-data_to_send = 0
+# Configure TCP socket
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket
+tcp_address = (TCP_SERVER_IP, TCP_SERVER_PORT)  # Define the address and port for TCP connection
+tcp_sock.connect(tcp_address)  # Connect the TCP socket to the defined address
 
+# Configure UDP socket
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a UDP socket
+udp_address = (UDP_SERVER_IP, UDP_SERVER_PORT)  # Define the address and port for UDP connection
+
+data_to_send = 0  # Initialize data to be sent
+
+# Main loop for sending data via TCP and UDP
 while True:
-    # Envoi des données via TCP
-    tcp_sock.send("{}".format(data_to_send))
-    print("Données envoyées via TCP: {}".format(data_to_send))
+    # Send data via TCP
+    tcp_sock.send("{}".format(data_to_send))  # Send data through the TCP socket
+    print("Data sent via TCP: {}".format(data_to_send))
     
-    # Envoi des données via UDP
-    udp_sock.sendto("{}".format(data_to_send), udp_address)
-    print("Données envoyées via UDP: {}".format(data_to_send))
+    # Send data via UDP
+    udp_sock.sendto("{}".format(data_to_send), udp_address)  # Send data through the UDP socket
+    print("Data sent via UDP: {}".format(data_to_send))
     
+    # Increment data and reset after reaching 1000
     data_to_send += 1
-    utime.sleep(1.2)  # Attendre une seconde entre chaque envoi
-    if (data_to_send==1000):
-        data_to_send=0
+    if data_to_send == 1000:
+        data_to_send = 0
+    
+    utime.sleep(1.2)  # Wait for 1.2 seconds before sending the next data
+        #To make the different tests we var
